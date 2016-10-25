a5a7f43195276fc33aee78f277b58a780b3439b5
Sean Gorman <seanedwardgorman@gmail.com>
2016-10-25 16:35:41 -0700

    doing pretty good so far, see if you can mess around and make it actually work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,66 @@
-function Player(name, score, totalScore) {
+function Player(name, roll, score, total, playing) {
   this.name = name;
+  this.roll = roll;
   this.score = score;
-  this.totalScore = totalScore;
+  this.total = total
+  this.playing = playing;
+  }
 
-  Player.prototype.roll = function() {
-   this.score= (Math.floor(Math.random() * 6) +1);
+Player.prototype.rollDie = function()  {
+     this.roll =  Math.floor((Math.random() * 6) +1);
 }
 
-    var totalScore = this.score += this.score;
+Player.prototype.addRollToScore = function() {
+    if (this.roll === 1) {
+      this.score === 0;
+      this.playing === false;
+    } else
+      this.score += this.roll;
 
+Player.prototype.addToTotal = function() {
+  this.total += this.score;
 }
+
+
+
+
+
+var player1 = new Player("Adam", 0, 0, 0, true);
+var player2 = new Player("Eve", 0, 0, 0, false);
+
+
 
 
 $(document).ready(function() {
-  $("#roll-button").click(function(event) {
+  $("button#roll").click(function(event) {
     event.preventDefault();
-    $('#user-score').append(" ");
-      console.log("what are you doing");
+    if (player1.playing === true) {
+      $(player1.rollDie());
+      $(player1.addRollToScore());
+        if (player1.playing === false) {
+          $(player1.addToTotal());
+          $("#player1Score").text(player1.total);
+        }
+    } else {
+      $(player2.rollDie());
+      $(player2.addRollToScore());
+      if (player2.playing === false) {
+        $(player2.addToTotal());
+        $("#player2").text(player2.total)
+      }
+    }
+  });
 
-
-
-
-
-
-
-
-
-});
+  $("button#hold").click(function(event) {
+    event.preventDefault();
+    if (player1.playing === true) {
+      $(player1.addToTotal());
+      $(player1.playing === false);
+      $("#player1Score").text(player1.total)
+    }else {
+      $(player2.addToTotal())
+      $(player2.playing === false);
+      $("#player2Score").text(player2.total);
+    }
+  });
 });
