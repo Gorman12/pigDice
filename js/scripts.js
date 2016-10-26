5f2cf718d476de2be173c2100cfc9f921f362da4
Sean Gorman <seanedwardgorman@gmail.com>
2016-10-26 00:36:18 -0700

    1 player rools die and die value is added to score value. new score value outputs onto webpage.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
-function Player(name, roll, score, total, playing) {
+function Player(name, roll, score, playing) {
   this.name = name;
   this.roll = roll;
   this.score = score;
-  this.total = total
   this.playing = playing;
   }
 
@@ -11,24 +10,11 @@ Player.prototype.rollDie = function()  {
 }
 
 Player.prototype.addRollToScore = function() {
-    if (this.roll === 1) {
-      this.score === 0;
-      this.playing === false;
-    } else {
       this.score += this.roll;
     }
-}
 
-Player.prototype.addToTotal = function() {
-  this.total += this.score;
-}
+var player1 = new Player("Adam", 0, 0, true);
 
-
-
-
-
-var player1 = new Player("Adam", 0, 0, 0, true);
-var player2 = new Player("Eve", 0, 0, 0, false);
 
 
 
@@ -39,31 +25,26 @@ $(document).ready(function() {
     if (player1.playing === true) {
       $(player1.rollDie());
       $(player1.addRollToScore());
-        if (player1.playing === false) {
-          $(player1.addToTotal());
-          $("#player1Score").text(player1.total);
-        }
-    } else {
-      $(player2.rollDie());
-      $(player2.addRollToScore());
-      if (player2.playing === false) {
-        $(player2.addToTotal());
-        $("#player2").text(player2.total)
-      }
-    }
-  });
-
-
-  $("button#hold").click(function(event) {
-    event.preventDefault();
-    if (player1.playing === true) {
-      $(player1.addToTotal());
-      $(player1.playing === false);
-      $("#player1Score").text(player1.total)
-    }else {
-      $(player2.addToTotal())
-      $(player2.playing === false);
-      $("#player2Score").text(player2.total);
+      alert(player1.score);
     }
   });
 });
+
+
+//   $("button#hold").click(function(event) {
+//     event.preventDefault();
+//
+//   // var player1 = new Player("Adam", 0, 0, 0, true);
+//   // var player2 = new Player("Eve", 0, 0, 0, false);
+//
+//     if (player1.playing === true) {
+//       $(player1.addToTotal());
+//       $(player1.playing === false);
+//       $("#player1Score").text(player1.total)
+//     }else {
+//       $(player2.addToTotal())
+//       $(player2.playing === false);
+//       $("#player2Score").text(player2.total);
+//     }
+//   });
+// });
