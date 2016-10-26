b5f2de44bad196453c3edcb28ef7fd30610324bb
Sean Gorman <seanedwardgorman@gmail.com>
2016-10-26 14:56:57 -0700

    game logic succesfully handles both users score, total, playing and roll values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,33 @@
-function Player(name, roll, score, playing) {
+function Player(name, roll, score, total, playing) {
   this.name = name;
   this.roll = roll;
   this.score = score;
+  this.total = total;
   this.playing = playing;
   }
 
 Player.prototype.rollDie = function()  {
-     this.roll =  Math.floor((Math.random() * 6) +1);
+  this.roll =  Math.floor((Math.random() * 6) +1);
 }
 
 Player.prototype.addRollToScore = function() {
-      this.score += this.roll;
-    }
+  this.score += this.roll;
+}
 
-var player1 = new Player("Adam", 0, 0, true);
+Player.prototype.addScoreToTotal = function() {
+  this.total += this.score;
+}
 
+Player.prototype.togglePlayer = function() {
+  this.playing = !this.playing;
+}
+
+Player.prototype.scoreToZero = function() {
+  this.score = 0;
+}
+
+var player1 = new Player("Adam", 0, 0, 0, true);
+var player2 = new Player("eve", 0, 0, 0, false)
 
 
 
@@ -24,27 +37,38 @@ $(document).ready(function() {
     event.preventDefault();
     if (player1.playing === true) {
       $(player1.rollDie());
-      $(player1.addRollToScore());
-      alert(player1.score);
+      if (player1.roll === 1) {
+        $(player1.scoreToZero());
+        $("#player1Score").text(player1.score);
+        $("#player1Roll").text(player1.roll);
+        $(player1.togglePlayer());
+      } else {
+        $(player1.addRollToScore());
+        $("#player1Score").text(player1.score);
+        $("#player1Roll").text(player1.roll);
+      }
+    } else {
+      $(player2.rollDie());
+      if (player2.roll === 1) {
+        $(player2.scoreToZero());
+        $("#player2Score").text(player2.score)
+        $("#player2Roll").text(player2.roll);
+        $(player1.togglePlayer());
+      } else {
+        $(player2.addRollToScore());
+        $("#player2Score").text(player2.score);
+        $("#player2Roll").text(player2.roll);
+      }
     }
   });
+  $("button#hold").click(function(event) {
+    event.preventDefault();
+    $(player1.addScoreToTotal());
+    $(player2.addScoreToTotal());
+    $(player1.scoreToZero());
+    $(player2.scoreToZero());
+    $(player1.togglePlayer());
+    $("#player1Total").text(player1.total);
+    $("#player2Total").text(player2.total);
+  });
 });
-
-
-//   $("button#hold").click(function(event) {
-//     event.preventDefault();
-//
-//   // var player1 = new Player("Adam", 0, 0, 0, true);
-//   // var player2 = new Player("Eve", 0, 0, 0, false);
-//
-//     if (player1.playing === true) {
-//       $(player1.addToTotal());
-//       $(player1.playing === false);
-//       $("#player1Score").text(player1.total)
-//     }else {
-//       $(player2.addToTotal())
-//       $(player2.playing === false);
-//       $("#player2Score").text(player2.total);
-//     }
-//   });
-// });
